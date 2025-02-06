864ad7593eea594d73e53a3a671396370b39cc47
Nikmorking <nikmor.2010.king@gmail.com>
2025-02-06 19:06:29 +0300

    1

--- nft/scripts/portal.gd
+++ nft/scripts/portal.gd
@@ -1,19 +1,20 @@
-extends Node2D
+extends Area2D
 
-var mob
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-func _input(event: InputEvent) -> void:
-	pass
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func _on_timer_timeout() -> void:
-	mob = load("res://сцены/Воин.tscn").instantiate()
-	get_parent().add_child(mob)
-	mob.global_position = global_position
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		if body.tipy == "Player":
+			
+			pass
+		pass
 	pass # Replace with function body.
