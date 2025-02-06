53edb535950ce5f9d2bf6d759af9f4c8b9d8b67b
Nikmorking <nikmor.2010.king@gmail.com>
2025-02-06 18:23:10 +0300

    1

--- nft/scripts/spawner.gd
+++ nft/scripts/spawner.gd
@@ -1,19 +1,17 @@
 extends Node2D
 
+var mob
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("spawn"):
-		var mob = load("res://сцены/Воин.tscn").instantiate()
-		get_parent().add_child(mob)
-		mob.global_position = global_position
-	pass
-
-
-
+	mob = load("res://сцены/Воин.tscn").instantiate()
+	get_parent().add_child(mob)
+	mob.global_position = global_position
+	$Timer.start()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
