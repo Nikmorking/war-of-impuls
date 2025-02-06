ff1a43c70603c55d0d37c9c156f4d1ccdc70be5a
Nikmorking <nikmor.2010.king@gmail.com>
2025-02-06 18:40:47 +0300

    1

--- nft/scripts/spawner.gd
+++ nft/scripts/spawner.gd
@@ -7,11 +7,14 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func _input(event: InputEvent) -> void:
-	mob = load("res://сцены/Воин.tscn").instantiate()
-	get_parent().add_child(mob)
-	mob.global_position = global_position
-	$Timer.start()
-	
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_timer_timeout() -> void:
+	mob = load("res://сцены/Воин.tscn").instantiate()
+	get_parent().add_child(mob)
+	mob.global_position = global_position
+	pass # Replace with function body.
