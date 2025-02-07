a26cbe4a42daf32675d9551a7c5221206da38de1
Nikmorking <nikmor.2010.king@gmail.com>
2025-02-07 11:10:22 +0300

    mob v3

--- nft/scripts/spawner.gd
+++ nft/scripts/spawner.gd
@@ -6,8 +6,14 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("spawn"):
-		var mob = load("res://сцены/Воин.tscn").instantiate()
+	var mob
+	if Input.is_action_just_pressed("spawn_1"):
+		mob = load("res://сцены/Воин.tscn").instantiate()
+	if Input.is_action_just_pressed("spawn_2"):
+		mob = load("res://сцены/лучник.tscn").instantiate()
+	if Input.is_action_just_pressed("spawn_3"):
+		mob = load("res://сцены/Маг.tscn").instantiate()
+	if mob:
 		get_parent().add_child(mob)
 		mob.global_position = global_position
 	pass
