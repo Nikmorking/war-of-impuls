49bdc395a0bc7ef54cbbbb45d25336b13b3ec46c
TimKud62 <tim008182@gmail.com>
2025-02-10 15:37:45 +0300

    plushku
    кастет фараона

---
+++ nft/сцены/сушки.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@export var vampirism = 10
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		if body.tipy == "Player":
+			print("va")
+			queue_free()
+
+pass
