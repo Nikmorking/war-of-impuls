cdf40a388489e206fa2afb612caed08a0287b0e2
TimKud62 <tim008182@gmail.com>
2025-03-04 19:26:22 +0300

    xp

--- nft/scripts/плюшка.gd
+++ nft/scripts/плюшка.gd
@@ -10,6 +10,7 @@ func _on_body_entered(body: Node2D) -> void:
 			if(body.max_health != body.health):
 				xp = body.health
 				xp *= 0.22
+				xp = round(xp) 
 				body.health = body.health + xp
 				body.vis_health()
 				queue_free()
