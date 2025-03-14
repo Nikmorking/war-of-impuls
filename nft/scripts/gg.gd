7f23f311ff4e8e022d92c12b87f540cf2c456d5f
Nikmorking <nikmor.2010.king@gmail.com>
2025-03-14 19:34:05 +0300

    dial start

--- nft/scripts/gg.gd
+++ nft/scripts/gg.gd
@@ -1,7 +1,22 @@
 extends Node
-
+var manik = false
+signal _vis_ui
+signal _vis_man
+signal _open_door
 
 func get_papa(col: float, sel: Node) -> Node:
 	for i in col:
 		sel = sel.get_parent()
 	return sel
+
+func vis_ui():
+	_vis_ui.emit()
+	pass
+
+func open_door():
+	_open_door.emit()
+	pass
+
+func vis_man():
+	_vis_man.emit()
+	pass
