aa45645d8c9809b36a24493347bce0837b713085
Nikmorking <nikmor.2010.king@gmail.com>
2025-03-14 21:02:01 +0300

    В

--- nft/scripts/Entity.gd
+++
@@ -1,7 +0,0 @@
-extends CharacterBody2D
-
-@export var max_health = 100
-var health = 100
-@export var SPEED = 10 
-@export var tipy: String 
-@export var damage = 10
--- nft/scripts/gg.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-
-func get_papa(col: float, sel: Node) -> Node:
-	for i in col:
-		sel = sel.get_parent()
-	return sel
