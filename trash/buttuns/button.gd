d8b9b79a71a7f9a1de2abe26471969ff1e33cbe6
Nikmorking <nikmor.2010.king@gmail.com>
2025-03-28 19:53:05 +0300

    wt

---
+++ trash/buttuns/button.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_button_down():
+	$AnimatedSprite2D.play("down")
+	pass # Replace with function body.
+
+
+func _on_button_button_up():
+	$AnimatedSprite2D.play("up")
+	pass # Replace with function body.
